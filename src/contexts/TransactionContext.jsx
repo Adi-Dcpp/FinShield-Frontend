f90ad232d4afffbc60f718d6930571c51f083e62
Adi <luffy1120adi@gmail.com>
2026-04-01 22:42:37 +0530

    feat: added the transaction router and useTransaction hooks

--- src/contexts/TransactionContext.jsx
+++ src/contexts/TransactionContext.jsx
@@ -1,1 +1,35 @@
 import { createContext } from "react";
+import {
+    reviewTransaction,
+    proceedTransaction,
+    declineTransaction,
+    history
+} from "../services/Api";
+
+const TransactionContext = createContext();
+
+const TransactionProvider = ({ children }) => {
+    const review = async (data) => {
+        return await reviewTransaction(data);
+    }
+
+    const proceed = async (data) => {
+        return await proceedTransaction(data);
+    }
+
+    const decline = async (data) => {
+        return await declineTransaction(data);
+    }
+
+    const getHistory = async (data) => {
+        return await history(data);
+    }
+
+    return (
+        <TransactionContext.Provider value={{ review, proceed, decline, getHistory }}>
+            {children}
+        </TransactionContext.Provider>
+    )
+}
+
+export { TransactionContext, TransactionProvider };
